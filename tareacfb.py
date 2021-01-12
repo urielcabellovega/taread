18cc473a964f6d6326e01a072fe2bc1ceab04e1a
urielcabellovega <42848722+urielcabellovega@users.noreply.github.com>
2021-01-11 18:46:52 -0600

    Add files via upload

--- tareacfb.py
+++ tareacfb.py
@@ -10,7 +10,6 @@ caracteres= []
 caracteres2=[]
 pruebap=[]
 contadorc=[]
-c=0
 for linea in fic:
     for c in linea:
         if(c!=' ' and c!='\n' and c!= '\xad'):
@@ -29,35 +28,27 @@ print(caracteres2)
 print("\t====================================================================")
 print("\tSEPARACION DEL TEXTO EN CADENAS DE LONGITUD DE LA PALABRA A BUSCAR")
 print("\t====================================================================")
-for indice in range(len(caracteres)-len(caracteres2)):
-    for c in range(len(caracteres2)):   
-        pruebap.append(caracteres[indice+c])
-        
-    if (pruebap==caracteres2):
-        contadorc.append(indice)
-        c=c+1
-    print("",indice+1,pruebap,end="")
-    pruebap=[]
 
+for indice in range(len(caracteres)-len(caracteres2)):
+    if (caracteres2[0]==caracteres[indice]):
+        for c in range(len(caracteres2)):   
+            pruebap.append(caracteres[indice+c])
+        print("",indice+1,pruebap,end="")
+    if (pruebap==caracteres2):
+            contadorc.append(indice)
+            
+    pruebap=[]    
+
+    
 print("\n\t=============================================")
 print("\tPOSICIONES EN LAS QUE SE ENCUENTRA LA PALABRA",caracteres2)
 print("\t=============================================")
 if(len(contadorc)!=0):
     for indice in range(len(contadorc)):
-        print("\nLa ocurrencia",indice+1,"se encuentra en la posicion:",contadorc[indice]+1)
+        print("\nLa ocurrencia",indice+1,"se encuentra en la posicion:",\
+              contadorc[indice]+1)
 if(len(contadorc)==0):
     print("No se encontro la cadena")
 
-'''
-    pruebap= (caracteres[indice],caracteres[indice+1],caracteres[indice+2],\
-              caracteres[indice+3])
-    if (pruebap==caracteres2):
-        contadorc.append(i)
-        c=c+1
-    print(pruebap,end="")
-print()
-print()
-print(contadorc)
-#print("La ocurrencia",c,"se encuentra en la posicion",contadorc[c])
-'''
 fic.close()
+
